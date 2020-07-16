7d73c4fcc8d878e411c1edc5e7bf45b0b79047a8
Paul Gratz <pgratz@gratz1.com>
2020-07-16 15:54:46 -0500

    minor changes

--- Lab01/main.c
+++ Lab01/main.c
@@ -2,11 +2,13 @@
 
 #include <stdio.h>
 
+/* "test" is our function that is written in ARMv8 Assembly */
 extern long long int test(long long int a, long long int b);
 
 int main(void)
 {
-    long long int a = test(3, 5);
+    long long int a = test(3, 5); /* Calling assembly funct test from
+				     a C program */
     printf("Result of test(3, 5) = %lld\n", a);
     return 0;
 }
