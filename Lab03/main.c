b2a2f33fb726b023c5dd721644ef517aa402a170
Rohit Barichello <rohitbarichello@gmail.com>
2020-09-21 13:24:07 -0500

    Lab03 finished

--- Lab03/main.c
+++ Lab03/main.c
@@ -3,10 +3,16 @@
 #include <stdio.h>
 
 extern long long int test(long long int a, long long int b);
+extern long long int lab03b();
+extern long long int lab03c();
 
 int main(void)
 {
-    long long int a = test(3, 5);
-    printf("Result of test(3, 5) = %ld\n", a);
+    long long int b = lab03b();
+    printf("Lab3b ouput: %d\n", b);
+    
+    long long int c = lab03c();
+    printf("Lab3c ouput: %d\n", c);
+    
     return 0;
 }
